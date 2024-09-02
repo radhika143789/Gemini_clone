724a6443aa5243d316cae348d972e55c70743e7a
Radhika <radhikakhattar07@gmail.com>
2024-09-02 22:20:48 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import Sidebar from "./Components/Sidebar/Sidebar";
+import React from 'react';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Main from './Components/Main/Main';
 const App = () => {
   return(
   <>
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -15,29 +15,41 @@ const Main = () => {
                 </div>
                 <div className="cards">
                     <div className="card">
-                        <p>Suggest some music</p>
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
                         <img src={assests.compass_icon} alt="" />
 
                     </div>
                     <div className="cards">
                         <div className="card">
                             <p>Suggest some music</p>
-                            <img src={assests.compass_icon} alt="" />
+                            <img src={assests.bulb_icon} alt="" />
 
                         </div>
                         <div className="cards">
                             <div className="card">
-                                <p>Suggest some music</p>
-                                <img src={assests.compass_icon} alt="" />
+                                <p>Brainstrom team bonding activities for our work retreat</p>
+                                <img src={assests.message_icon} alt="" />
 
                             </div>
                             <div className="cards">
                                 <div className="card">
-                                    <p>Suggest some music</p>
-                                    < img src={assests.compass_icon} alt="" />
+                                    <p>Improve the readability of the following code</p>
+                                    < img src={assests.code_icon} alt="" />
                                 </div>
 
                             </div>
+                            <div className="main-bottom">
+                                <div classsName="search-box">
+                                    <input type="text" placeholder='Enter a prompt here' />
+                                    <div>
+                                        <img src={assets.gallery_icon} alt="" />
+                                        <img src={assets.mic_icon} alt="" />
+                                        <img src={assets.send_icon} alt="" />
+                                    </div>   
+                                    </div>
+                                    <p className="botton-info"></p>
+                            </div>
+
                         </div>
                     </div>
 
