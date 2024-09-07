ed6971394ff981fb76cf8ff0dd6dc3c96a617feb
Radhika <radhikakhattar07@gmail.com>
2024-09-07 20:50:11 +0530

    api relates changes

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,45 +1,62 @@
-import React, { useState } from 'react';
+import React, { useContext} from 'react';
 import './Main.css';
 import { assets } from '../../assets/assets';
-
+import { Context } from '../../context/context';
 const Main = () => {
-    return ( 
+    const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context)
+    return (
         <div className='main'>
             <div className="nav">
                 <p>Gemini</p>
                 <img src={assets.user_icon} alt="User Icon" />
             </div>
             <div className="main-container">
-                <div className="greet">
-                    <p><span>Hello, Dev</span></p>
-                    <p>How can I help you today</p>
-                </div>
-                <div className="cards">
-                    <div className="card">
-                        <p>Suggest beautiful places to see on an upcoming road trip</p>
-                        <img src={assets.compass_icon} alt="Compass Icon" /> {/* Fixed typo from `assests` to `assets` */}
+                {!showResult
+                    ? <>
+                        <div className="greet">
+                            <p><span>Hello, Dev</span></p>
+                            <p>How can I help you today</p>
+                        </div>
+                        <div className="cards">
+                            <div className="card">
+                                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                                <img src={assets.compass_icon} alt="Compass Icon" /> {/* Fixed typo from `assests` to `assets` */}
+                            </div>
+                            <div className="card"> {/* Fixed nested div class names */}
+                                <p>Suggest some music</p>
+                                <img src={assets.bulb_icon} alt="Bulb Icon" /> {/* Fixed typo from `assests` to `assets` */}
+                            </div>
+                            <div className="card">
+                                <p>Brainstorm team bonding activities for our work retreat</p> {/* Fixed typo "Brainstrom" to "Brainstorm" */}
+                                <img src={assets.message_icon} alt="Message Icon" /> {/* Fixed typo from `assests` to `assets` */}
+                            </div>
+                            <div className="card">
+                                <p>Improve the readability of the following code</p>
+                                <img src={assets.code_icon} alt="Code Icon" /> {/* Fixed typo from `assests` to `assets` */}
+                            </div>
+                        </div>
+                    </>
+                    : <div className='result'>
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt="" />
+                            <p>{recentPrompt}</p>
+
+                        </div>
+                        <div className="result-data">
+                            <img src={assets.gemini_icon} alt="" />
+                            <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                        </div>
                     </div>
-                    <div className="card"> {/* Fixed nested div class names */}
-                        <p>Suggest some music</p>
-                        <img src={assets.bulb_icon} alt="Bulb Icon" /> {/* Fixed typo from `assests` to `assets` */}
-                    </div>
-                    <div className="card">
-                        <p>Brainstorm team bonding activities for our work retreat</p> {/* Fixed typo "Brainstrom" to "Brainstorm" */}
-                        <img src={assets.message_icon} alt="Message Icon" /> {/* Fixed typo from `assests` to `assets` */}
-                    </div>
-                    <div className="card">
-                        <p>Improve the readability of the following code</p>
-                        <img src={assets.code_icon} alt="Code Icon" /> {/* Fixed typo from `assests` to `assets` */}
-                    </div>
-                </div>
+                }
+
                 <div className="main-bottom">
-                    <div className="search-box"> {/* Fixed typo from `classsName` to `className` */}
-                        <input type="text" placeholder='Enter a prompt here' />
+                    <div className="search-box"> { }
+                        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here' />
                         <div>
                             <img src={assets.gallery_icon} alt="Gallery Icon" />
                             <img src={assets.mic_icon} alt="Mic Icon" />
-                            <img src={assets.send_icon} alt="Send Icon" />
-                        </div>   
+                            <img onClick={() => onSent} src={assets.send_icon} alt="Send Icon" />
+                        </div>
                     </div>
                     <p className="bottom-info"> {/* Fixed typo from `botton-info` to `bottom-info` */}
                         Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps.
